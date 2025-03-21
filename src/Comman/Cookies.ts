24f83f7502e6725a9640aeb30c8247c20c679734
Harshad Katariya <harshad2043@gmail.com>
2025-03-21 10:01:02 +0530

    Cookies.ts

--- src/Comman/Cookies.ts
+++ src/Comman/Cookies.ts
@@ -64,19 +64,4 @@ class cookieParser
 }
 
 export const CookieParser = new cookieParser()
-// parseDoctorCookies (request) {
-//     var list = {},
-//     rc = request.headers.cookie;
-//     if(rc !== undefined && rc !== ""){
-//         rc && rc.split(';').forEach(function( cookie ) {
-//             var parts = cookie.split('=');
-//             list[parts.shift().trim()] = decodeURI(parts.join('='));
-//         });
-//         if(list[process.env.DOCTOR_AUTH_COOKIE] !== undefined && list[process.env.DOCTOR_AUTH_COOKIE] !== "")
-//             return list[process.env.DOCTOR_AUTH_COOKIE];
-//         else
-//             return null
-//     }else{
-//         return null
-//     }
-//}
+
